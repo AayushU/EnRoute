b99eb2a5aa72e6f6a4f9c738f1d522f05ab689ab
Aayush Upadhyay <aayushu@gmail.com>
2012-12-02 20:38:22 -0500

    Added search button which leads to results activity, which displays three tabs.

--- src/com/example/enroute/MainActivity.java
+++ src/com/example/enroute/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.enroute;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
 
 public class MainActivity extends Activity {
 	
@@ -20,6 +22,20 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.activity_main, menu);
 		return true;
+	}
+	
+	@Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		super.onActivityResult(requestCode, resultCode, data);
+            
+        if(resultCode == 1) {  // ResultsActivity
+        } 
+    }
+	
+	
+	public void displayResults (View view) {
+		Intent intent = new Intent(this, ResultsActivity.class);
+    	startActivityForResult(intent, 1);
 	}
 
 }
