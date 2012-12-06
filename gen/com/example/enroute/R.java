71ec660f6ebb0f34290a0e2168ed54bf73add040
Bay <bgross17@gmail.com>
2012-12-06 02:28:52 -0500

    building out main activity!

--- gen/com/example/enroute/R.java
+++ gen/com/example/enroute/R.java
@@ -12,18 +12,22 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int map_bg=0x7f020001;
     }
     public static final class id {
-        public static final int button1=0x7f070000;
-        public static final int container=0x7f070001;
-        public static final int directions_button=0x7f070008;
-        public static final int label=0x7f070002;
-        public static final int label2=0x7f070004;
-        public static final int label3=0x7f070005;
-        public static final int label4=0x7f070006;
-        public static final int label5=0x7f070007;
-        public static final int menu_settings=0x7f070009;
-        public static final int toshow=0x7f070003;
+        public static final int button1=0x7f070003;
+        public static final int container=0x7f070004;
+        public static final int destinationInput=0x7f070001;
+        public static final int directions_button=0x7f07000b;
+        public static final int label=0x7f070005;
+        public static final int label2=0x7f070007;
+        public static final int label3=0x7f070008;
+        public static final int label4=0x7f070009;
+        public static final int label5=0x7f07000a;
+        public static final int menu_settings=0x7f07000c;
+        public static final int queryInput=0x7f070002;
+        public static final int searchTitle=0x7f070000;
+        public static final int toshow=0x7f070006;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
--- src/com/example/enroute/MainActivity.java
+++ src/com/example/enroute/MainActivity.java
@@ -1,20 +1,47 @@
 package com.example.enroute;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
 import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends Activity {
-	
-	//Avi is adding a comment
-	//Comment number 2
 
+  //-------------------------------------------------------
+  // Instance Variables
+  
+  Handler searchHandler;
+  ProgressDialog searchDialog;
+  Context mainContext;
+  
+  //Private attributes
+  private EditText destinationInput;
+  private EditText queryInput;
+  
+  //-------------------------------------------------------
+  // onCreate
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		
+		//initialize the view
 		setContentView(R.layout.activity_main);
+		
+    //find our UI elements
+    destinationInput = (EditText) findViewById(R.id.queryInput);
+    queryInput = (EditText) findViewById(R.id.destinationInput);
+    
+		//initialize instance vars
+		searchHandler = new Handler();
+		mainContext = this;
+		
 	}
 
 	@Override
@@ -25,7 +52,7 @@ public class MainActivity extends Activity {
 	}
 	
 	@Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+  protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 		super.onActivityResult(requestCode, resultCode, data);
             
         if(resultCode == 1) {  // ResultsActivity
@@ -33,9 +60,59 @@ public class MainActivity extends Activity {
     }
 	
 	
-	public void displayResults (View view) {
-		Intent intent = new Intent(this, ResultsActivity.class);
-    	startActivityForResult(intent, 1);
+  //-------------------------------------------------------
+  // search onClick handler
+	public void searchOnClick (View view) {
+	  
+	  //show dialogue box
+	  searchDialog = ProgressDialog.show(MainActivity.this, "", "Loading...", true);
+	  
+	  //and fire background event 
+    try {
+      System.out.println("Calling backgroundSearchTask()");
+      new backgroundSearchTask().execute();
+    } catch(Exception e){
+      Log.v("lookup failed","Exception:"+e.getMessage());
+    }
 	}
+	
+	
+
+	//-------------------------------------------------------
+	//asynchronous task for results lookup
+  private class backgroundSearchTask extends AsyncTask<String, Integer, String> {
+    
+    //run our background task
+    protected String doInBackground(String... trash) {
+
+      //call the main search algorithm
+      String destination = destinationInput.getText().toString();
+      String query = queryInput.getText().toString();
+      //loadSearchResults( destination, query);
+      
+      String retVal = "";
+      return retVal;
+    }   
+    
+    //after background task completes
+    protected void onPostExecute(String result) {
+      
+      //fire event back to main handler
+      searchHandler.postDelayed(new Runnable() {
+         public void run() {
+           
+           //close the progress dialog
+           searchDialog.dismiss();
+           
+           //and start new intent to show results page
+           Intent intent = new Intent(mainContext, ResultsActivity.class);
+           startActivityForResult(intent, 1);
+
+         }
+      }, 0);  //0ms 
+
+    }
+  }
+	  
 
 }
