68326987cd887ba3e4495fff939270ab73aa8127
Bay <bgross17@gmail.com>
2012-12-12 12:55:54 -0500

    cleaning list display

--- src/com/example/enroute/Constants.java
+++ src/com/example/enroute/Constants.java
@@ -8,6 +8,5 @@ public class Constants {
   static final GeoPoint GROVE_CEMETERY = new GeoPoint(41312972, -72928244);
   static final GeoPoint ELM_YORK       = new GeoPoint(41310695, -72929944);
   static final int      DEFAULT_ZOOM   = 18;
-  static final String AAYUSH_API_KEY = "AIzaSyBoRZcwm_ad2YpAdLlA6XKn8m8Gjae9B0E";
-  
+  static final String AAYUSH_API_KEY = "AIzaSyBoRZcwm_ad2YpAdLlA6XKn8m8Gjae9B0E";  
 }
